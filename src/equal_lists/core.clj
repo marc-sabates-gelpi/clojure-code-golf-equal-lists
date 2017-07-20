101b1fabd1afd7144840b3dd0938a7b01b972296
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2017-07-20 19:55:02 +0100

    Use multimethods

--- src/equal_lists/core.clj
+++ src/equal_lists/core.clj
@@ -1,17 +1,29 @@
 (ns equal-lists.core
   (:gen-class))
 
-(defn leq [x y]
-  (cond
-   (and (coll? x) (coll? y)) (and
-                              (leq
-                               (first x)
-                               (first y))
-                              (leq
-                               (next x)
-                               (next y)))
-   (not (or (coll? x) (coll? y))) (= x y)
-   :else false))
+(defmulti leq (fn [x y]
+                (cond
+                 (and (coll? x) (coll? y)) :all-colls
+                 (not (or (coll? x) (coll? y))) :none-colls
+                 :else :mixed-colls)))
+
+(defmethod leq :all-colls
+  [x y]
+  (and
+   (leq
+    (first x)
+    (first y))
+   (leq
+    (next x)
+    (next y))))
+
+(defmethod leq :none-colls
+  [x y]
+  (= x y))
+
+(defmethod leq :mixed-colls
+  [_ _]
+  false)
 
 (defn -main
   [x y & args]
